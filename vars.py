9c8a358b391d35e0d46bb078464883be3c317ea6
11abhibaghel-ux <11abhibaghel@gmail.com>
2025-10-01 12:28:06 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,8 +1,8 @@
-api_id = 22779671
-api_hash = "125d8d88b77309dc3b154cbbfc2dacb2"
-bot_token = "6948815452:AAHLdIf8gKLDp_MMkt8iSJ6wLOTdsAsIq-I"
-auth_users = [1713924419]
-sudo_user = 1713924419
+api_id = 21651155
+api_hash = "ab2760e4187293ef21efdae20f6477c3"
+bot_token = "8188034266:AAFg1euLWbNoyF5eF_dhNbDAAfSilnZhhKk"
+auth_users = [5126701813]
+sudo_user = 5126701813
 log_channel = -1002143629860
 txt_channel = -1002049391187
 
